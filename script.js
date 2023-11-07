594cb2fe19fd1ef207c1690591e9130971cb6089
Bagi <roland.bagi007@gmail.com>
2023-11-07 11:29:43 +0100

    oda vissza szinez

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ for(let i = 0; i < 64; i++){
     content.innerHTML += `<div class="box" id="${i+1}"></div>`;
 }
 //NodeList-ként megkapjuk a .box-okat:
+
 const boxes = document.querySelectorAll(".box");
+
+
 //Végig megyünk az egyes div-eken, és rájuk akasztunk egy-egy eseményfigyelőt:
+
 boxes.forEach(element => {
     //Az eseményfigyelő a click eseményre indul be:
     element.addEventListener("click", (event) => {
-        event.target.style.backgroundColor = "#ff0000";
+        console.log(element);
+        let currentElement = event.target;
+        if(currentElement.style.backgroundColor === ""){
+            currentElement.style.backgroundColor = "#DFFF00";
+            currentElement.textContent = currentElement.id;
+        }
+        else{
+            currentElement.style.backgroundColor = "";
+            currentElement.textContent = "";
+        }
+        //currentElement.style.backgroundColor = "#DFFF00";
+        currentElement.style.textAlign = "center"
+        currentElement.style.color = "purple"
     })
 })
